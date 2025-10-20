4531b5bdb133aae3e7c2c0661f8f94ad64277f95
rolandslt <lulangoroland46@gmail.com>
2025-10-20 13:50:38 +0200

    correct Customers to start with uppercase in Orders tabel

--- task_2.sql
+++ task_2.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Order_Details table
