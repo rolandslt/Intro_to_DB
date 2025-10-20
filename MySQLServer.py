cb271d873fafd8c795415850596e854b524d0403
rolandslt <lulangoroland46@gmail.com>
2025-10-20 11:12:31 +0200

    Add MySQLServer.py to create alx_book_store database

---
+++ MySQLServer.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Ro16#05$99@",
+        
+    )
+
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+        print("Database 'als_bo0k_store' created successfully! ")
+
+except Error as e:
+    print(f"Failed to connect ot the  database : {e}" )
+
+
+mycursor.close()
+mydb.close()
